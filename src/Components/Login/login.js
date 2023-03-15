bbe3778ea015ad00b58b214612820771ed157875
Olya <okatranis@gmail.com>
2023-03-15 13:58:09 +0200

    fix local storage (login) test

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -7,11 +7,19 @@ const Login = () => {
     const [ user, setUser ] = useState(null);
     const [ profile, setProfile ] = useState(null);
 
+    useEffect(() => {
+        const userName = localStorage.getItem('userName');
+        if(userName) {
+            setProfile({name: userName});
+        }
+    }, [])
+
 
     const login = useGoogleLogin({
         onSuccess: (codeResponse) => {
             setUser(codeResponse);
             localStorage.setItem('userName', codeResponse.profileObj.name);
+            setProfile({ name: codeResponse.profileObj.name });
         },
         onError: (error) => console.log('Login Failed:', error)
     });
@@ -43,12 +51,6 @@ const Login = () => {
         localStorage.removeItem('userName');
     };
 
-    useEffect(() => {
-        const userName = localStorage.getItem('userName');
-        if(userName) {
-            setProfile({name: userName});
-        }
-    }, [])
 
     return (
         <div>
